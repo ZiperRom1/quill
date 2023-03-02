0c3627f3f5324e3a7a0cccdd4c19dd3401a010e1
Romain Laneuville <romain.laneuville@hotmail.fr>
2023-03-02 23:14:08 +0100

    [scripts] - useless export CC and CXX vars

--- examples/test_shared.sh
+++ examples/test_shared.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 CURRENT_DIR=$(dirname "${BASH_SOURCE[0]}")
-# Set compiler
-export CC=/usr/local/bin/x86_64-w64-mingw32-gcc
-export CXX=/usr/local/bin/x86_64-w64-mingw32-g++
 # Build
 cmake -S "${CURRENT_DIR}" -B build_shared -DCMAKE_TOOLCHAIN_FILE="${CURRENT_DIR}/mingw-w64-x86_64.cmake" -DBUILD_SHARED_LIBS=ON
 cmake --build build_shared --target example_trivial_shared
--- examples/test_static.sh
+++ examples/test_static.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 CURRENT_DIR=$(dirname "${BASH_SOURCE[0]}")
-# Set compiler
-export CC=/usr/local/bin/x86_64-w64-mingw32-gcc
-export CXX=/usr/local/bin/x86_64-w64-mingw32-g++
 # Build
 cmake -S "${CURRENT_DIR}" -B build_static -DCMAKE_TOOLCHAIN_FILE="${CURRENT_DIR}/mingw-w64-x86_64.cmake"
 cmake --build build_static --target example_trivial_static
